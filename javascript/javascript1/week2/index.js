e8a46b7c7b9b6e0d862001dfab6e55f23edcf7c2
Sana_Shabeel <sanastrobarre99@gmail.com>
2021-11-03 04:24:30 +0100

    Weather wear

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -14,3 +14,17 @@ const fullname1 = getFullname(' anas ', 'Barre')
 const fullname2 = getFullname( ' Anisa ', 'Barre')
 console.log(fullname1);
 console.log(fullname2);
+
+
+// Weather wear
+
+function weatherWear (temp) {
+    if (temp > 30){
+        console.log(`Shorts and a t-shirt`)
+    } else {
+        console.log(`its going to be a cold one consider wearing a jacket  and trousers`)
+    }
+}
+weatherWear(50)
+
+
